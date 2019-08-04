0d7ecb0fba69afb0fc0e6ee9e98509daa7ed5e4b
amita-shukla <amitashukla0906@gmail.com>
2019-08-04 07:44:22 +0530

    override toString

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,5 +1,7 @@
 object Demo extends App {
-  class Amount(value: Double, currency: String)
+  class Amount(value: Double, currency: String){
+    override def toString: String = s"Amount($value,$currency)"
+  }
   val twoDollars = new Amount(2,"USD")
-  println(twoDollars) // prints Demo$Amount@13c27452
+  println(twoDollars) // prints Amount(2.0,USD)
 }
