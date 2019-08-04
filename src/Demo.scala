b8943a74f864facb12e584e96a8c6d0815e7e0b3
amita-shukla <amitashukla0906@gmail.com>
2019-08-04 07:49:10 +0530

    access class parameters outside of class

--- src/Demo.scala
+++ src/Demo.scala
@@ -1,7 +1,14 @@
 object Demo extends App {
-  class Amount(value: Double, currency: String){
+  class Amount(val value: Double,val currency: String){
+//    val amountValue = this.value
+//    val amountCurrency = this.currency
     override def toString: String = s"Amount($value,$currency)"
   }
   val twoDollars = new Amount(2,"USD")
-  println(twoDollars) // prints Amount(2.0,USD)
+//  println(twoDollars.amountValue)
+//  println(twoDollars.amountCurrency)
+
+  // the parameters are accessible on addding val keyword in class
+  println(twoDollars.value)
+  println(twoDollars.currency)
 }
