fe07ec2fe5f10a50aa6af12218724c09db69fa49
amita-shukla <amitashukla0906@gmail.com>
2019-08-04 14:16:27 +0530

    add companion object

--- src/Demo.scala
+++ src/Demo.scala
@@ -3,19 +3,16 @@ object Demo extends App {
     override def toString: String = s"Amount($value,$currency)"
   }
 
-  // object creates a singleton object for utility methods
-  // these methods need not be instantiated everytime you create an amount instance
-  object AmountUtils {
-    def convert(from: Amount, to:String): Amount = {
-      val conversionRate = 69.45
-      new Amount(from.value* conversionRate, to)
-    }
+  // companion object
+  object Amount{
+    def apply(value: Double, currency : String): Amount = new Amount(value,currency)
+    def apply(value: Double): Amount = new Amount(value,"USD")
   }
 
-  import AmountUtils._
   val twoDollars = new Amount(2,"USD")
-//  val twoDollarsEqINR = AmountUtils.convert(twoDollars,"INR")
-  val twoDollarsEqINR = convert(twoDollars,"INR")
-  println(twoDollarsEqINR)
+  val twoUSDollars = Amount(2)
+
+  println(twoDollars)
+  println(twoUSDollars)
 
 }
